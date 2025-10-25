372cd1411915ed8a105b7049359aa1a2d55cbed0
Kars <k@kars.bio>
2025-10-25 19:31:38 +0100

    feat: public stuff

---
+++ public/fonts/fontExports.ts
@@ -0,0 +1,56 @@
+import localFont from "next/font/local";
+
+/*
+Copyright © 2025 Kars (github.com/kars1996)
+
+Not to be shared, replicated or used without prior consent.
+Contact Kars for any enquiries
+*/
+
+export const Satoshi = localFont({
+    src: "./Satoshi.ttf",
+    variable: "--font-satoshi",
+    weight: "300 900",
+});
+
+export const GeistVF = localFont({
+    src: "./GeistVF.woff",
+    variable: "--font-geist",
+    weight: "100 900",
+});
+
+export const GeistMonoVF = localFont({
+    src: "./GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "300 900",
+});
+
+export const GeneralSans = localFont({
+    src: "./GeneralSans.ttf",
+    variable: "--font-general-sans",
+    weight: "200 700",
+});
+
+export const Raleway = localFont({
+    src: "./Raleway.ttf",
+    variable: "--font-raleway",
+    weight: "100 900",
+});
+
+export const Neon = localFont({
+    src: "./Neon.ttf",
+    variable: "--font-neon",
+    weight: "200 800",
+});
+
+export const Argon = localFont({
+    src: "./Argon.ttf",
+    variable: "--font-argon",
+    weight: "200 800",
+});
+
+export const Walsheim = localFont({
+    src: "./Walsheim.woff2",
+    variable: "--font-walsheim",
+    weight: "200 800",
+});
